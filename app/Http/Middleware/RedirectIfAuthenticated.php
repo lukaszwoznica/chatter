6ee04fd41b49c65931d785e32b582c5dd134bc40
Łukasz Woźnica <lukasz.l.woznica@gmail.com>
2021-03-27 19:16:03 +0100

    Laravel Fortify with Laravel Sanctum authentication

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -2,10 +2,10 @@
 
 namespace App\Http\Middleware;
 
-use App\Providers\RouteServiceProvider;
 use Closure;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Symfony\Component\HttpFoundation\Response;
 
 class RedirectIfAuthenticated
 {
@@ -23,7 +23,7 @@ class RedirectIfAuthenticated
 
         foreach ($guards as $guard) {
             if (Auth::guard($guard)->check()) {
-                return redirect(RouteServiceProvider::HOME);
+                return response()->json(['error' => 'Already authenticated.'], Response::HTTP_OK);
             }
         }
 
