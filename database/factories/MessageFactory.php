f035b3d8c3d0a44505f62357fef131e05bc790dd
Łukasz Woźnica <lukasz.l.woznica@gmail.com>
2021-03-29 12:44:42 +0200

    Message model, migration and factory

---
+++ database/factories/MessageFactory.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Message;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class MessageFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Message::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        $users = User::pluck('id')->toArray();
+        do {
+            $sender = $this->faker->randomElement($users);
+            $recipient = $this->faker->randomElement($users);
+        } while ($sender === $recipient);
+
+        return [
+            'sender_id' => $this->faker->randomElement($users),
+            'recipient_id' => User::factory(),
+            'text' => $this->faker->text
+        ];
+    }
+
+    /**
+     * Indicate that the message has been read
+     *
+     * @return MessageFactory
+     */
+    public function read()
+    {
+        return $this->state(function (array $attributes) {
+            return [
+                'read_at' => now(),
+            ];
+        });
+    }
+}
