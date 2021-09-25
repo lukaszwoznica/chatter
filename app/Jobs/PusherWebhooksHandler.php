acaf9a82f0115530bfbedde6037c3ca06a55c4a9
Łukasz Woźnica <lukasz.l.woznica@gmail.com>
2021-09-25 13:10:20 +0200

    Pusher webhook handler function refactoring

--- app/Jobs/PusherWebhooksHandler.php
+++ app/Jobs/PusherWebhooksHandler.php
@@ -17,19 +17,37 @@ class PusherWebhooksHandler extends ProcessWebhookJob
 
         collect($payload['events'])->each(function ($event) use ($userService) {
             $channelSegments = explode('.', $event['channel'], 2);
-
-            if ($channelSegments[0] === 'private-messages') {
-                $data['is_online'] = $event['name'] === 'channel_occupied';
-                if (!$data['is_online']) {
-                    $data['last_online_at'] = now();
-                }
-
-                $user = User::find($channelSegments[1]);
-                $user->update($data);
-
-                $userContacts = $userService->getUserContacts($user);
-                Notification::send($userContacts, new UserOnlineStatusChangedNotification($user));
+            if ($channelSegments[0] !== 'private-messages') {
+                return;
             }
+
+            $data = $this->prepareUserOnlineStatusDataForUpdate($event['name']);
+            $user = $this->updateUserOnlineStatus(intval($channelSegments[1]), $data);
+            $this->notifyUserContacts($user, $userService);
         });
+    }
+
+    private function prepareUserOnlineStatusDataForUpdate(string $eventType): array
+    {
+        $data['is_online'] = $eventType === 'channel_occupied';
+        if (!$data['is_online']) {
+            $data['last_online_at'] = now();
+        }
+
+        return $data;
+    }
+
+    private function updateUserOnlineStatus(int $userId, array $data): User
+    {
+        $user = User::find($userId);
+        $user->update($data);
+
+        return $user;
+    }
+
+    private function notifyUserContacts(User $user, UserService $userService): void
+    {
+        $userContacts = $userService->getUserContacts($user);
+        Notification::send($userContacts, new UserOnlineStatusChangedNotification($user));
     }
 }
