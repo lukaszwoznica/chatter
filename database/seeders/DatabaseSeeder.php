9ee95460a0a3c7d283da9e240e149d87deeaa9ef
Łukasz Woźnica <lukasz.l.woznica@gmail.com>
2022-01-21 16:08:53 +0100

    Database seeder setup

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,17 +2,33 @@
 
 namespace Database\Seeders;
 
+use App\Models\Message;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
     /**
      * Seed the application's database.
-     *
-     * @return void
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        $this->createAdminAndTestUser();
+        User::factory()->count(20)->create();
+        Message::factory()->count(10000)->create();
+    }
+
+    private function createAdminAndTestUser()
+    {
+        User::factory()->admin()->create([
+            'first_name' => 'Chatter',
+            'last_name' => 'Admin',
+            'email' => 'admin@chatter.xyz'
+        ]);
+        User::factory()->create([
+            'first_name' => 'John',
+            'last_name' => 'Doe',
+            'email' => 'user@chatter.xyz'
+        ]);
     }
 }
